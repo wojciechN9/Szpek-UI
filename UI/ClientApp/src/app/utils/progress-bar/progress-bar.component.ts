4d75f6a516de659a477732c5a1757630a5a76614
Wojtek <papusniper@gmail.com>
2019-11-24 15:20:23 +0100

    add progress-bar when map loading

---
+++ UI/ClientApp/src/app/utils/progress-bar/progress-bar.component.ts
@@ -0,0 +1,23 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'progress-bar',
+  templateUrl: './progress-bar.component.html',
+  styleUrls: ['./progress-bar.component.css']
+})
+export class ProgressBarComponent {
+  public pendingChanges = 0;
+
+  public addChange() {
+    this.pendingChanges++;
+  }
+
+  public removeChange() {
+    this.pendingChanges--;
+  }
+
+  public error() {
+    this.pendingChanges = 0;
+    window.alert('błąd ładowania');
+  }
+}
