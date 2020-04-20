d34da7a17183f3fad04976fa0267a8170fda0eea
Wojtek <papusniper@gmail.com>
2020-04-20 12:02:41 +0200

    move text resources to separate file

--- UI/ClientApp/src/app/l10n-config.ts
+++ UI/ClientApp/src/app/l10n-config.ts
@@ -1,20 +1,28 @@
 import { L10nConfig, L10nLoader } from "angular-l10n";
-import { textResources } from "./text-resources";
+import { enTextResources } from "./text-resources/en-text-resources";
+import { plTextResources } from "./text-resources/pl-text-resources";
+
+function getTextResources() {
+  return {
+    'en-US': enTextResources,
+    'pl-PL': plTextResources
+  }
+}
 
 export const l10nConfig: L10nConfig = {
-    format: 'language-region',
-    providers: [
-      { name: 'app', asset: textResources }
-    ],
-    cache: true,
-    keySeparator: '.',
-    defaultLocale: { language: 'en-US', currency: 'USD' },
-    schema: [
-        { locale: { language: 'en-US', currency: 'USD' }, dir: 'ltr', text: 'United States' },
-        { locale: { language: 'pl-PL', currency: 'PLN' }, dir: 'ltr', text: 'Poland' }
-    ]
+  format: 'language-region',
+  providers: [
+    { name: 'app', asset: getTextResources() }
+  ],
+  cache: true,
+  keySeparator: '.',
+  defaultLocale: { language: 'en-US', currency: 'USD' },
+  schema: [
+    { locale: { language: 'en-US', currency: 'USD' }, dir: 'ltr', text: 'United States' },
+    { locale: { language: 'pl-PL', currency: 'PLN' }, dir: 'ltr', text: 'Poland' }
+  ]
 };
 
 export function initL10n(l10nLoader: L10nLoader): () => Promise<void> {
-    return () => l10nLoader.init();
+  return () => l10nLoader.init();
 }
