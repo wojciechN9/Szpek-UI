d22938646b465178dc00ba3254e8e059cb52efb1
Wojtek <papusniper@gmail.com>
2020-05-12 11:15:37 +0200

    calendar fixes

--- UI/ClientApp/src/app/shared/components/meassurements-calendar/meassurements-calendar.component.ts
+++ UI/ClientApp/src/app/shared/components/meassurements-calendar/meassurements-calendar.component.ts
@@ -1,60 +1,49 @@
-import { Component, Input, OnChanges, Inject } from "@angular/core";
-import { NgbDateStruct, NgbDate } from "@ng-bootstrap/ng-bootstrap";
+import { Component, Input, Inject, OnInit } from "@angular/core";
+import { NgbDate } from "@ng-bootstrap/ng-bootstrap";
 import { Meassurement } from "../../../location-meassurements/meassurement.type";
 import { MeassurementsHttpService } from "../../../utils/http-services/meassurements.http.service";
 import { AirQualityEnum } from "../../../location-meassurements/air-quality.type";
 import { getAirQualityText, getAirQualityColor } from "../../../utils/enum/air-quality";
 import { L10N_LOCALE, L10nLocale } from "angular-l10n";
 
-
 @Component({
   selector: 'meassurements-calendar',
   templateUrl: './meassurements-calendar.component.html'
 })
-export class MeassurementsCalendarComponent implements OnChanges {
+
+export class MeassurementsCalendarComponent implements OnInit {
   @Input() locationId: number;
-  @Input() initialMeassurementsData: Array<Meassurement>;
   public calendarMeassurements: Array<Meassurement>;
   public loaderActive = false;
-  public currentCalendarDay: NgbDateStruct;
-  public initialCalendarDay: NgbDateStruct;
-  public dataFetched = false;
+  public currentDay: NgbDate;
+  public today: NgbDate;
 
   constructor(
     private meassurementsService: MeassurementsHttpService,
     @Inject(L10N_LOCALE) public locale: L10nLocale) { }
 
-  ngOnChanges() {
-    if (this.initialMeassurementsData !== null && this.dataFetched === false) {
-      this.calendarMeassurements = this.initialMeassurementsData;
 
-      if (this.initialMeassurementsData.length !== 0)
-        this.initialCalendarDay = {
-          year: new Date(this.initialMeassurementsData[0].periodTo).getFullYear(),
-          month: new Date(this.initialMeassurementsData[0].periodTo).getMonth() + 1,
-          day: new Date(this.initialMeassurementsData[0].periodTo).getDate()
-        };
-      else {
-        let today = new Date();
-        this.initialCalendarDay = {
-          year: today.getFullYear(),
-          month: today.getMonth() + 1,
-          day: today.getDate()
-        };
-      }
-      this.currentCalendarDay = this.initialCalendarDay;
+  ngOnInit() {
+    const today = new Date();
+    this.today = new NgbDate(today.getFullYear(), today.getMonth() + 1, today.getDate());
+    this.currentDay = this.today;
 
-      this.dataFetched = true;
+    if (this.locationId !== null) {
+      this.onDateSelected(this.today);
     }
   }
 
   onDateSelected(ngbDate: NgbDate) {
     this.loaderActive = true;
-    this.meassurementsService.getMeassurementsByDate(this.locationId, new Date(ngbDate.year, ngbDate.month - 1, ngbDate.day).toISOString())
+    this.meassurementsService.getMeassurementsByDate(this.locationId, this.convertToDate(ngbDate).toISOString())
       .subscribe((result) => {
         this.calendarMeassurements = result;
         this.loaderActive = false;
       });
+  }
+
+  convertToDate(ngbDate: NgbDate): Date {
+    return new Date(ngbDate.year, ngbDate.month - 1, ngbDate.day);
   }
 
   getQualityText(airQuality: AirQualityEnum) {
