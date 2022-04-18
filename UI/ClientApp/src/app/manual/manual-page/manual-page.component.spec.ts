fc5b7053c6c6b2afc16a5c0ab3608b812da5fbc2
Wojtek <you@example.com>
2022-04-18 12:27:24 +0200

    updated angular to v11

--- UI/ClientApp/src/app/manual/manual-page/manual-page.component.spec.ts
+++ UI/ClientApp/src/app/manual/manual-page/manual-page.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ManualPageComponent } from './manual-page.component';
 
@@ -6,7 +6,7 @@ describe('ManualPageComponent', () => {
   let component: ManualPageComponent;
   let fixture: ComponentFixture<ManualPageComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ManualPageComponent ]
     })
