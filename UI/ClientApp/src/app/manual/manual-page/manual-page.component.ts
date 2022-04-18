e21c6d6ba343de0dcbc664e066e4677e1571d938
Wojtek <you@example.com>
2022-04-18 11:00:11 +0200

    add manual in the new module

---
+++ UI/ClientApp/src/app/manual/manual-page/manual-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manual-page',
+  templateUrl: './manual-page.component.html',
+  styleUrls: ['./manual-page.component.css']
+})
+export class ManualPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
